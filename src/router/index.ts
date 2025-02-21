58e7600387f4b4f17ac453977c12e7f42ccbadcd
rocsoar_mac <lsyyd3454@outlook.com>
2025-02-21 22:17:37 +0800

    first

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import {createRouter, createWebHistory} from 'vue-router';
+import type {RouteRecordRaw} from 'vue-router';
+
+const history = createWebHistory();
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/front/index',
+        name: '/front/index',
+        component: () => import('../views/front/index.vue')
+    },
+    {
+        path: '/mis/login',
+        name: '/mis/login',
+        component: () => import('../views/mis/login.vue')
+    }
+];
+
+const router = createRouter({
+    history,
+    routes
+});
+
+export default router;
--- src/views/front/index.vue
+++ src/views/front/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
-  <router-view/>
+  <div>index</div>
 </template>
 
 <style scoped>
+
 </style>
